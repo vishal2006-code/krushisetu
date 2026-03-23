8307ba575e169cf3b9386d93e373dff922e9290b
Vishal <vishal@gmail.com>
2026-03-23 09:26:36 +0530

    final cors fix absolute

--- server.js
+++ server.js
@@ -22,13 +22,22 @@ const app = express();
 // ---------------- DB ----------------
 connectDB();
 
-// ---------------- CORS ----------------
-app.use(cors());
-app.options(/.*/, cors());
+// ---------------- CORS FIX ----------------
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  next();
+});
+
+// handle preflight
+app.options("*", (req, res) => {
+  res.sendStatus(200);
+});
 
 // ---------------- Middleware ----------------
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("combined"));
 
 const limiter = rateLimit({
